c740cf580e7d36d1aeaa4e3e0793a7e806dbc6c3
Gopi95533 <155801476+Gopi95533@users.noreply.github.com>
2024-04-30 12:33:21 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DropdownlistEx.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            &nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label1" runat="server" Text="Select Country"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="186px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label2" runat="server" Text="Select State"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged" Width="192px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label3" runat="server" Text="Select City"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList3" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList3_SelectedIndexChanged" Width="217px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace DropdownlistEx
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private void GetCountries()
+        {
+          
+           // SqlConnection con = new SqlConnection("data source=GOPI;database=ddldemo;integrated security=yes");
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Constr"].ToString());
+            SqlDataAdapter da = new SqlDataAdapter("select * from country ", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds,"country");
+            DropDownList1.DataSource = ds;
+            DropDownList1.DataTextField = "countryname";
+            DropDownList1.DataValueField= "countryid";
+            DropDownList1.DataBind();
+            DropDownList1.Items.Insert(0, "select Coumtry");
+        }
+        private void GetStates()
+        {
+           
+            //SqlConnection con = new SqlConnection("data source=GOPI;database=ddldemo;integrated security=yes");
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Constr"].ToString());
+            string query ="select s.stateid,s.statename from country c inner join state s on c.countryid=s.countryid Where c.countryname='"+DropDownList1.SelectedItem.ToString()+"'";
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "state");
+            DropDownList2.DataSource = ds;
+            DropDownList2.DataTextField = "statename";
+            DropDownList2.DataValueField = "stateid";
+            DropDownList2.DataBind();
+            DropDownList2.Items.Insert(0, "select State");
+        }
+        private void GetCitys()
+        {
+           
+          // SqlConnection con = new SqlConnection("data source=GOPI;database=ddldemo;integrated security=yes");
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Constr"].ToString());
+            string query ="select c.cityid,c.cityname from state s inner join city c on s.stateid=c.stateid where s.statename='"+DropDownList2.SelectedItem.ToString()+"'";
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "city");
+            DropDownList3.DataSource = ds;
+            DropDownList3.DataTextField = "cityname";
+            DropDownList3.DataValueField = "cityid";
+            DropDownList3.DataBind();
+            DropDownList3.Items.Insert(0, "select City");
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack) 
+            {
+                GetCountries();
+
+            }
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetStates();
+
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetCitys();
+
+        }
+
+        protected void DropDownList3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+           //SqlConnection con = new SqlConnection("data source=GOPI;database=ddldemo;integrated security=yes");
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Constr"].ToString());
+            string query = "select e1.eno,e1.ename,e1.salary from emp e1 inner join city c on e1.cityid=c.cityid where c.cityname='" + DropDownList3.SelectedItem.ToString() + "'";
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "emp");
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+        }
+    }
+}
